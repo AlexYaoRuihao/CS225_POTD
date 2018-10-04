18dab385e0e1d4b43f65736380a4e15c4a0cce90
Ruihao Yao <alex.yaorh@gmail.com>
2018-10-03 19:15:57 -0500

    gg

--- potd-q22/Node.cpp
+++ potd-q22/Node.cpp
@@ -2,9 +2,45 @@
 
 using namespace std;
 
+bool isPresent(Node *head, int data){
+  Node *temp = head;
+  while(temp != NULL){
+    if(temp->data_ == data){
+      return true;
+    }
+    temp = temp->next_;
+  }
+  return false;
+}
+
+void createNew_and_insertFront(Node *&result, Node &other){
+  Node *newNode = new Node(other);
+
+  if(result == NULL){
+    result = newNode;
+    newNode->next_ = NULL;
+  }
+  else{
+    //Node *temp = result;
+    newNode->next_ = result;
+    result = newNode;
+  }
+}
+
+
+
 Node *listIntersection(Node *first, Node *second) {
   // your code here
-  return NULL;
+  Node *result = NULL;
+  Node *temp_first = first;
+
+  while(temp_first != NULL){
+    if(isPresent(second, temp_first->data_)){
+      createNew_and_insertFront(result, *temp_first);
+    }
+    temp_first = temp_first->next_;
+  }
+  return result;
 }
 
 Node::Node() {
