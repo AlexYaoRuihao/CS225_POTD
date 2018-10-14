a9c6bf888b69b1f208587e6eb74e535c779d0f62
Ruihao Yao <alex.yaorh@gmail.com>
2018-10-14 12:33:54 -0500

    gg

--- potd-q29/TreeNode.cpp
+++ potd-q29/TreeNode.cpp
@@ -1,9 +1,58 @@
 #include "TreeNode.h"
 #include <iostream>
 
+TreeNode *findMaximum(TreeNode *croot){
+  if(croot == NULL){
+    return NULL;
+  }
+  while(croot -> right_ != NULL){
+    croot = croot -> right_;
+  }
+  return croot;
+}
+
 TreeNode * deleteNode(TreeNode* root, int key) {
   // your code here
+  //return root;
+  if(root == NULL){
+    return root;
+  }
+
+  if(root -> val_ > key){
+    root -> left_ = deleteNode(root -> left_, key);
+  }
+  else if(root -> val_ < key){
+    root -> right_ = deleteNode(root -> right_, key);
+  }
+  else{
+    //no child case
+    if(root -> left_ == NULL && root -> right_ == NULL){
+      delete root;
+      root = NULL;
+    }
+    //one child case
+    else if(root -> left_ == NULL){
+      TreeNode *temp = root;
+      root = root -> right_;
+      delete temp;
+    }
+    else if(root -> right_ == NULL){
+      TreeNode *temp = root;
+      root = root -> left_;
+      delete temp;
+    }
+    //two children case
+    else{
+      TreeNode *temp = findMaximum(root -> left_);//IOS
+      root -> val_ = temp -> val_;
+      root ->left_ = deleteNode(root -> left_, temp -> val_);
+    }
+
+  }
   return root;
+
+
+
 }
 
 void inorderPrint(TreeNode* node)
