cd304b1d6f55303e50e48760501cf9100527fd79
Ruihao Yao <alex.yaorh@gmail.com>
2018-10-16 00:29:02 -0500

    gg

--- potd-q30/HuffmanUtils.cpp
+++ potd-q30/HuffmanUtils.cpp
@@ -1,8 +1,15 @@
 #include "HuffmanNode.h"
 #include "HuffmanUtils.h"
 #include <string>
+#include <map>
+
 
 using std::string;
+using namespace std;
+
+map<char, string> codes;
+
+//map<char, string> codes;
 
 /**
  * binaryToString
@@ -15,7 +22,35 @@ using std::string;
 
 string binaryToString(string binaryString, HuffmanNode* huffmanTree) {
     /* TODO: Your code here */
-    return "";
+    //return "";
+    string answer = "";
+    HuffmanNode *current = huffmanTree;
+    for(size_t i = 0; i < binaryString.length(); i++){
+      if(binaryString[i] == '0'){
+        current = current -> left_;
+      }
+      else{
+        current = current ->right_;
+      }
+      if(current -> left_ == NULL && current -> right_ == NULL){
+        answer+=(current -> char_);
+        current = huffmanTree;
+      }
+    }
+    return answer;
+}
+
+
+void store(HuffmanNode *root, string str){
+  if(root == NULL){
+    return;
+  }
+  if(root -> char_ != '@'){
+    codes[root->char_] = str;
+  }
+  store(root->left_, str+"0");
+  store(root->right_, str+"1");
+  return;
 }
 
 /**
@@ -30,5 +65,21 @@ string binaryToString(string binaryString, HuffmanNode* huffmanTree) {
 
 string stringToBinary(string charString, HuffmanNode* huffmanTree) {
     /* TODO: Your code here */
-    return "";
+    string answer = "";
+    store(huffmanTree, answer);
+    for(string::iterator it = charString.begin(); it != charString.end(); it++){
+      answer+=codes[*it];
+    }
+    return answer;
 }
+
+
+
+
+
+
+
+
+
+
+///////////////////
