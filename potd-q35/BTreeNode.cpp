2ad14667b6e7768a9cd1f63409e5ec0aa7ffa37e
Ruihao Yao <alex.yaorh@gmail.com>
2018-10-22 16:57:08 -0500

    gg

--- potd-q35/BTreeNode.cpp
+++ potd-q35/BTreeNode.cpp
@@ -3,6 +3,20 @@
 
 
 BTreeNode* find(BTreeNode* root, int key) {
-    // Your Code Here
-  return NULL;
+  // Your Code Here
+  //return NULL;
+  unsigned i;
+  for(i = 0; i < root->elements_.size() && key > root->elements_[i]; i++){}
+
+  if(i < root->elements_.size() && key == root->elements_[i]){
+    return root;
+  }
+
+  if(root->is_leaf_){
+    return NULL;
+  }
+  else{
+    BTreeNode *nextChild = root->children_[i];
+    return find(nextChild, key);
+  }
 }
