562f1b9dc36a30d8605d8306a128a9df433a269c
Ruihao Yao <alex.yaorh@gmail.com>
2018-10-22 18:31:19 -0500

    gg

--- potd-q35/BTreeNode.h
+++ potd-q35/BTreeNode.h
@@ -7,8 +7,9 @@
 
 struct BTreeNode {
     bool is_leaf_=true;
-    std::vector<int> elements_;
+    std::vector<int> elements_;//vector of keys of current node
     std::vector<BTreeNode*> children_;
+    //vector of children of current node
     BTreeNode() {}
     BTreeNode (std::vector<int> v) {
       this->elements_ = v;
