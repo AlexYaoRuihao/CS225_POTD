0b5234dc8ad30d157d04e1678c309b1214c302dc
Ruihao Yao <alex.yaorh@gmail.com>
2018-10-26 16:56:12 -0500

    gg

--- potd-q39/Hash.cpp
+++ potd-q39/Hash.cpp
@@ -1,16 +1,40 @@
 #include <vector>
 #include <string>
 #include "Hash.h"
+#include <iostream>
+#include <map>
 
 using namespace std;
 
 int hashFunction(string s, int M) {
    // Your Code Here
    //hash function to sum up the ASCII characters of the letters of the string
+   int sum = 0;
+   for(unsigned i = 0; i < s.length(); i++){
+     sum += s[i];
+   }
+   return (sum % M);
  }
 
 int countCollisions (int M, vector<string> inputs) {
 	int collisions = 0;
 	// Your Code Here
+  vector<int> index_record;
+  map<int, int> good_map;
+  for(unsigned i = 0; i < inputs.size(); i++){
+    index_record.push_back(hashFunction(inputs[i], M));
+  }
+
+  for(unsigned i = 0; i < index_record.size(); i++){
+    good_map[index_record[i]]++;
+  }
+
+  for(auto &it : good_map){
+    if(it.second != 1){
+      collisions += (it.second - 1);
+    }
+    //cout<<it.first<<" : "<<it.second<<endl;
+  }
+
 	return collisions;
 }
