f0c2c8c764a2834c354df18af2465a4744982b9b
Ruihao Yao <ruihaoy2@siebl-0224-27.ews.illinois.edu>
2018-11-01 10:08:30 -0500

    gg

--- potd-q43/Hash.cpp
+++ potd-q43/Hash.cpp
@@ -3,6 +3,9 @@
 #include <algorithm>
 #include <vector>
 #include "Hash.h"
+#include <map>
+
+using namespace std;
 
 unsigned long bernstein(std::string str, int M)
 {
@@ -17,16 +20,28 @@ unsigned long bernstein(std::string str, int M)
 float hash_goodness(std::string str, int M)
 {
     std::vector<int>* array = new std::vector<int>(M);	// Hint: This comes in handy
-	int permutation_count = 0;	
+	int permutation_count = 0;
 	float goodness = 0;
+
+	map<unsigned long, int> good_map;
 	do {
 		if (permutation_count == M) break;
 		// Code for computing the hash and updating the array
+		unsigned long magic = bernstein(str, M);
+		(*array)[permutation_count] = magic;
+		permutation_count++;
+		good_map[magic]++;
 
 	} while(std::next_permutation(str.begin(), str.end()));
-	
+
 	//Code for determining goodness
+	int i = 0;
+	for(auto &it : good_map){
+		if(it.second != 1){
+			i += (it.second - 1);
+		}
+	}
+	goodness = (float)i/M;
 
 	return goodness;
 }
-
