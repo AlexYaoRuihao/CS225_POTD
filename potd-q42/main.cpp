a3dbe9b2e6d142af883a49c3e179ad0e62e8ff3f
Ruihao Yao <alex.yaorh@gmail.com>
2018-10-31 19:32:51 -0500

    gg

--- potd-q42/main.cpp
+++ potd-q42/main.cpp
@@ -5,15 +5,45 @@ using namespace std;
 
 int main() {
     unordered_map<string, int> mapA({
-                                    {"common", 13}, 
+                                    {"common", 13},
                                     {"unique_a", 13}
                                     });
     unordered_map<string, int> mapB({
-                                    {"common", 17}, 
+                                    {"common", 17},
+                                    //{"common", 18},
+                                    //{"common", 18},
                                     {"unique_b", 17}
                                     });
 
     unordered_map<string, int> common = common_elems(mapA, mapB);
 
     // check your outputs here
+    for(auto &it : common){
+      cout<<it.first<<" "<<it.second<<endl;
+    }
 }
+// int main ()
+// {
+//   std::unordered_map<std::string,std::string> mymap;
+//
+//   // populating container:
+//   mymap["U.S."] = "Washington";
+//   mymap["U.S."] = "London";
+//   mymap["France"] = "Paris";
+//   mymap["France"] = "fuck";
+//   mymap["Russia"] = "Moscow";
+//   mymap["China"] = "Beijing";
+//   mymap["Germany"] = "Berlin";
+//   mymap["Japan"] = "Tokyo";
+//
+//   // erase examples:
+//   mymap.erase ( mymap.begin() );      // erasing by iterator
+//   //mymap.erase ("France");             // erasing by key
+//   //mymap.erase ( mymap.find("China"), mymap.end() ); // erasing by range
+//
+//   // show content:
+//   for ( auto& x: mymap )
+//     std::cout << x.first << ": " << x.second << std::endl;
+//
+//   return 0;
+// }
--- potd-q42/potd.cpp
+++ potd-q42/potd.cpp
@@ -1,11 +1,90 @@
 #include <unordered_map>
+#include <iostream>
+#include <vector>
 
 using namespace std;
 
-unordered_map<string, int> common_elems(unordered_map<string, int> mapA,
-                                        unordered_map<string, int> mapB) {
+unordered_map<string, int> common_elems(unordered_map<string, int>& mapA,
+                                        unordered_map<string, int>& mapB) {
 
     // your code here
+    //return unordered_map<string, int>();
+   unordered_map<string, int> good_unordered_map;
+   vector<string> good_vector;
+  //   //////////////////////////////////////////////////////////////////////
+  //   for(unordered_map<string, int>::iterator it = mapA.begin(); it != mapA.end(); it++){
+  //     cout<<it->first<<" "<<it->second<<endl;
+  //   }
+  //   //////////////////////////////////////////////////////////////////////
+  //   for(unordered_map<string, int>::iterator itA = mapA.begin(); itA != mapA.end(); itA++){
+  //     for(unordered_map<string, int>::iterator itB = mapB.begin(); itB != mapB.end(); itB++){
+  //       if(itA->first == itB->first){
+  //         good_unordered_map[itA->first]+=itB->second;
+  //         //mapB.erase(itB);
+  //       }
+  //       //cout<<itB->first<<" "<<itB->second<<endl;
+  //     }
+  //     //////////////////////////////////////////////////////////////////////
+  //     // for(unordered_map<string, int>::iterator it = good_unordered_map.begin(); it != good_unordered_map.end(); it++){
+  //     //   cout<<it->first<<" "<<it->second<<endl;
+  //     // }
+  //     //////////////////////////////////////////////////////////////////////
+  //     for(unordered_map<string, int>::iterator it = good_unordered_map.begin(); it != good_unordered_map.end(); it++){
+  //       mapB.erase(it->first);
+  //     }
+  //     // if(mapA.count(itA->first) > 1){
+  //     //   for(unordered_map<string, int>::iterator itA1 = mapA.begin(); itA1 != mapA.end(); ++itA1){
+  //     //     if(itA->first == itA1->first){
+  //     //       good_unordered_map[itA->first]+=itA1->second;
+  //     //     }
+  //     //   }
+  //     //   mapA.erase(itA->first);
+  //     // }
+  //
+  //     if(good_unordered_map.count(itA->first) > 0 || mapA.count(itA->first) > 1){
+  //       for(unordered_map<string, int>::iterator itA1 = itA; itA1 != mapA.end(); itA1++){
+  //         if(itA->first == itA1->first){
+  //           good_unordered_map[itA->first]+=itA1->second;
+  //         }
+  //       }
+  //       //mapA.erase(itA->first);
+  //     }
+  //     for(unordered_map<string, int>::iterator it = good_unordered_map.begin(); it != good_unordered_map.end(); it++){
+  //       mapA.erase(it->first);
+  //     }
+  //     // if(good_unordered_map.count(itA->first) > 0){
+  //     //   good_unordered_map[itA->first]+=itA->second;
+  //     //   mapA.erase(itA);
+  //     // }
+  // }
+    for(unordered_map<string, int>::iterator itA = mapA.begin(); itA != mapA.end(); itA++){
+      for(unordered_map<string, int>::iterator itB = mapB.begin(); itB != mapB.end(); itB++){
+        if(itA->first == itB->first){
+          good_unordered_map[itA->first]+=itA->second;
+          good_unordered_map[itA->first]+=itB->second;
+          good_vector.push_back(itA->first);
+          break;
+        }
+      }
+    }
 
-    return unordered_map<string, int>();
-} 
+    for(unordered_map<string, int>::iterator it = good_unordered_map.begin(); it != good_unordered_map.end(); it++){
+      mapA.erase(it->first);
+      mapB.erase(it->first);
+    }
+    return good_unordered_map;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+//////////
