146d394232600908e53eda67df7252013ba4a375
Ruihao Yao <alex.yaorh@gmail.com>
2018-11-13 21:37:43 -0600

    gg

--- potd-q50/Friend.cpp
+++ potd-q50/Friend.cpp
@@ -1,11 +1,13 @@
 #include "Friend.h"
+#include <vector>
+
+using namespace std;
 
 int find(int x, std::vector<int>& parents) {
    // your code
+   if(parents[i] < )
 }
 
 int findCircleNum(std::vector<std::vector<int>>& M) {
     // your code
 }
-
-
