60f73dd3611f6a74f4684dbcb69304bcf7d07b2b
Ruihao Yao <alex.yaorh@gmail.com>
2018-11-14 16:15:59 -0600

    gg

--- potd-q50/Friend.cpp
+++ potd-q50/Friend.cpp
@@ -5,9 +5,19 @@ using namespace std;
 
 int find(int x, std::vector<int>& parents) {
    // your code
-   if(parents[i] < )
+   //if(parents[i] < )
+   return 0;
 }
 
 int findCircleNum(std::vector<std::vector<int>>& M) {
     // your code
+    int M_size = M.size();
+    for(unsigned i = 0; i < M.size(); i++){
+      for(unsigned j = 0; j < M[i].size(); j++){
+        if(M[i][j] == 1 && i > j){
+          M_size--;
+        }
+      }
+    }
+    return M_size;
 }
