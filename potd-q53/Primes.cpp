3788f976b140ed9e78ddf2ee290b3fd76dbd296c
Ruihao Yao <alex.yaorh@gmail.com>
2018-11-15 10:34:41 -0600

    gg

--- potd-q53/Primes.cpp
+++ potd-q53/Primes.cpp
@@ -1,10 +1,20 @@
 #include <vector>
 #include "Primes.h"
 
+using namespace std;
+
 std::vector<int> *genPrimes(int M) {
     std::vector<int> *v = new std::vector<int>();
     // your code here
-
+    //vector<bool> good_vector;
+    for(int i = 2; i <= M; i++){
+      v->push_back(i);
+      for(int j = 2; j < i; j++){
+        if(i % j == 0){
+          v->pop_back();
+          break;
+        }
+      }
+    }
     return v;
 }
-
