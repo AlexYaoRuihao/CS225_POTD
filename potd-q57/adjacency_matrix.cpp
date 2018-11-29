5dd29965b09a780e8cf34d7663cf5927af67db99
YaoSir <alexyao@wirelessprv-10-194-43-169.near.illinois.edu>
2018-11-28 19:20:30 -0600

    gg

--- potd-q57/adjacency_matrix.cpp
+++ potd-q57/adjacency_matrix.cpp
@@ -10,20 +10,38 @@ using namespace std;
  */
 
 bool containsEdge(Graph const * const g, int src, int dest) {
+    //return false;
+    if(g->adjMatrix[src][dest] == 1){
+      return true;
+    }
     return false;
 }
 
 void addEdge(Graph* g, int src, int dest) {
-
+    g->adjMatrix[src][dest] = 1;
 }
 
 int numOutgoingEdges(Graph const * const g, int v) {
-    return 0;
+    //return 0;
+    int count = 0;
+    for(int i = 0; i < g->n; i++){
+      if(containsEdge(g, v, i)){
+        count++;
+      }
+    }
+    return count;
 }
 
 
 int numIncomingEdges(Graph const * const g, int v) {
-    return 0;
+    //return 0;
+    int count = 0;
+    for(int i = 0; i < g->n; i++){
+      if(containsEdge(g, i, v)){
+        count++;
+      }
+    }
+    return count;
 }
 
 void printGraph(Graph const * const g) {
