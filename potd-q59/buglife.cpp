6ab1e6c8bed828bea4d613fc0c57de3a37990ce1
YaoSir <alexyao@wirelessprv-10-194-43-169.near.illinois.edu>
2018-12-03 16:25:30 -0600

    gg

--- potd-q59/buglife.cpp
+++ potd-q59/buglife.cpp
@@ -1,8 +1,26 @@
+#include<iostream>
+#include<vector>
 
+using namespace std;
 // returns true if a counterexample is found
 bool bugLife(int numBugs, vector<int> &b1, vector<int> &b2) {
     // code to make ignore the warning
     // delete and replace with your own.
-    int i = numBugs + b1[0] + b2[0];
-    if (i>0) return false; else return false;
+    // int i = numBugs + b1[0] + b2[0];
+    // if (i>0) return false; else return false;
+    cout<<"Number of bugs: "<<numBugs<<endl;
+    for(auto &it1: b1){
+      cout<<it1<<" ";
+    }
+    cout<<endl;
+    for(auto &it2: b2){
+      cout<<it2<<" ";
+    }
+    cout<<endl;
+
+
+
+
+
+    return true;
 }
