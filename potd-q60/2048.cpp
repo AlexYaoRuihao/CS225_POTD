00ccbdacc2e16ea008ac3b0f5a4b0c572854f8ac
Ruihao Yao <alex.yaorh@gmail.com>
2018-12-20 20:29:35 +0800

    gg

--- potd-q60/2048.cpp
+++ potd-q60/2048.cpp
@@ -1,3 +1,12 @@
+using namespace std;
+
+void swap(int &num1, int &num2){
+  int temp;
+  temp = num1;
+  num1 = num2;
+  num2 = temp;
+}
+
 
 void run2048(int puzzle[4][4], int dir) {
 
@@ -7,8 +16,136 @@ void run2048(int puzzle[4][4], int dir) {
     //1-up
     //2-right
     //3-down
+
+    // cout<<"test: "<<endl;
+    // cout<<puzzle[1][3]<<endl;
+
+    //going left
     if(dir == 0){
-      
+      //begin sliding
+      for(int i = 0; i <= 3; i++){
+        for(int j = 0; j <= 3; j++){
+
+          if(puzzle[i][j] != 0){
+            continue;
+          }
+
+          else{
+            int temp_j = j;
+            temp_j++;
+
+            while(temp_j < 4){
+              if(puzzle[i][temp_j] != 0){
+                swap(puzzle[i][j], puzzle[i][temp_j]);
+                break;
+              }
+              temp_j++;
+            }
+
+          }
+
+        }
+      }
+      //begin merging
+      for(int i = 0; i < 3; i++){
+        for(int j = 0; j < 2; j++){
+          
+        }
+      }
+    }
+
+    else if(dir == 1){//going up
+      //begin sliding
+      for(int j = 0; j <= 3; j++){
+        for(int i = 0; i <= 3; i++){
+
+          if(puzzle[i][j] != 0){
+            continue;
+          }
+
+          else{
+            int temp_i = i;
+            temp_i++;
+
+            while(temp_i < 4){
+              if(puzzle[temp_i][j] != 0){
+                swap(puzzle[i][j], puzzle[temp_i][j]);
+                break;
+              }
+              temp_i++;
+            }
+          }
+
+        }
+      }
+      //begin merging
+
+
+    }
+
+    else if(dir == 2){//going right
+      //begin sliding
+      for(int i = 0; i <= 3; i++){
+        for(int j = 3; j >= 0; j--){
+
+          if(puzzle[i][j] != 0){
+            continue;
+          }
+
+          else{
+            int temp_j = j;
+            temp_j--;
+
+            while(temp_j >= 0){
+              if(puzzle[i][temp_j] != 0){
+                swap(puzzle[i][j], puzzle[i][temp_j]);
+                break;
+              }
+              temp_j--;
+            }
+          }
+
+        }
+      }
+      //begin merging
+
+    }
+
+    else{//dir == 3, going down
+      //begin sliding
+      for(int j = 0; j <= 3; j++){
+        for(int i = 3; i >= 0; i--){
+
+          if(puzzle[i][j] != 0){
+            continue;
+          }
+
+          else{
+            int temp_i = i;
+            temp_i--;
+
+            while(temp_i >= 0){
+              if(puzzle[temp_i][j] != 0){
+                swap(puzzle[i][j], puzzle[temp_i][j]);
+                break;
+              }
+              temp_i--;
+            }
+          }
+
+        }
+      }
+      //begin merging
+
+
     }
 
 }
+
+
+
+
+
+
+
+//
--- potd-q60/main.cpp
+++ potd-q60/main.cpp
@@ -15,11 +15,31 @@ int main() {
 
     cin >> dir;
 
-    run2048(arr,dir);
+    cout<<endl;
+    cout<<endl;
+    cout<<endl;
+    cout<<endl;
+    cout<<"input matrix: "<<endl;
 
     for(int i=0; i<4; i++) {
       cout << arr[i][0] << " " << arr[i][1] << " " << arr[i][2] << " " << arr[i][3] << endl;
     }
+
+
+    run2048(arr,dir);
+
+    cout<<endl;
+    cout<<endl;
+    cout<<endl;
+    cout<<endl;
+    cout<<"output matrix: "<<endl;
+
+    for(int i=0; i<4; i++) {
+      cout << arr[i][0] << " " << arr[i][1] << " " << arr[i][2] << " " << arr[i][3] << endl;
+    }
+
+    // int i = 2;
+    // int j = 3;
+    // swap(i, j);
+    // cout<<i<<" "<<j<<endl;
 }
-
-
